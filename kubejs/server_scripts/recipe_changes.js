7675bf7d4c0d891d912ed26c74fbb4ab0d7adfb7
Red <redangel121@gmail.com>
2024-08-07 15:46:37 -0400

    recipe overhaul started

--- kubejs/server_scripts/recipe_changes.js
+++ kubejs/server_scripts/recipe_changes.js
@@ -1,3 +1,12 @@
+ServerEvents.tags("item", event => {
+    event.add("forge:dusts/salt", "pamhc2foodcore:saltitem")
+    event.add("create:crushed", "#create:crushed_raw_materials")
+    let materials = ["iron", "copper", "gold", "zinc", "nickel", "lithium", "osmium", "tin", "lead", "uranium", "crimson_iron", "azure_silver", "cloggrum", "froststeel"]
+    for (let each of materials) {
+        event.add("create:crushed/" + each, "create:crushed_raw_" + each)
+    }
+})
+
 ServerEvents.recipes(event => {
     let recipeID = ["agricraft:coal", "agricraft:diamond", "agricraft:emerald", "agricraft:quartz", "agricraft:netherite_scrap", "agricraft:amethyst_shard", "bhc:red_heart_patch", "bhc:yellow_heart_patch", "bhc:green_heart_patch", "bhc:blue_heart_patch", "bhc:red_heart_canister", "bhc:yellow_heart_canister", "bhc:green_heart_canister", "bhc:blue_heart_canister", "bhc:soul_heart_canister", "bhc:canister", "bhc:relic_apple", "computercraft:turtle_normal", "computercraft:turtle_normal_overlays/turtle_trans_overlay", "computercraft:turtle_normal_overlays/turtle_rainbow_overlay", "computercraft:turtle_advanced", "computercraft:turtle_advanced_upgrade", "computercraft:turtle_advanced_overlays/turtle_trans_overlay", "computercraft:turtle_advanced_overlays/turtle_rainbow_overlay", "create:crafting/kinetics/mechanical_drill", "createdeco:gold_coin", "createdeco:pressing/coins/gold_coin", "createdeco:gold_coinstack", "createdeco:netherite_coin", "createdeco:pressing/coins/netherite_coin", "createdeco:netherite_coinstack", "createdeco:brass_coin", "createdeco:pressing/coins/brass_coin", "createdeco:brass_coinstack", "createdeco:iron_coin", "createdeco:pressing/coins/iron_coin", "createdeco:iron_coinstack", "createdeco:copper_coin", "createdeco:pressing/coins/copper_coin", "createdeco:copper_coinstack", "createdeco:industrial_iron_coin", "createdeco:pressing/coins/industrial_iron_coin", "createdeco:industrial_iron_coinstack", "createdeco:zinc_coin", "createdeco:pressing/coins/zinc_coin", "createdeco:zinc_coinstack", "gateways:basic/blaze", "gateways:basic/enderman", "gateways:basic/slime", "gateways:endless/blaze", "gateways:emerald_grove", "gateways:hellish_fortress", "gateways:overworldian_nights", "lightmanscurrency:coinmint", "lightmanscurrency:gem_terminal", "lightmanscurrency:portable_gem_terminal", "lightmanscurrency:gem_terminal_swap", "lightmanscurrency:portable_gem_terminal_swap", "lightmanscurrency:slot_machine", "lightmanscurrency:traders/slot_machine", "lightmanscurrency:coin_chocolate_copper", "lightmanscurrency:coinpile_chocolate_copper", "lightmanscurrency:coinblock_chocolate_copper", "lightmanscurrency:coin_chocolate_iron", "lightmanscurrency:coinpile_chocolate_iron", "lightmanscurrency:coinblock_chocolate_iron", "lightmanscurrency:coin_chocolate_gold", "lightmanscurrency:coinpile_chocolate_gold", "lightmanscurrency:coinblock_chocolate_gold", "lightmanscurrency:coin_chocolate_emerald", "lightmanscurrency:coinpile_chocolate_emerald", "lightmanscurrency:coinblock_chocolate_emerald", "lightmanscurrency:coin_chocolate_diamond", "lightmanscurrency:coinpile_chocolate_diamond", "lightmanscurrency:coinblock_chocolate_diamond", "lightmanscurrency:coin_chocolate_netherite", "lightmanscurrency:coinpile_chocolate_netherite", "lightmanscurrency:coinblock_chocolate_netherite", "minecraft:ender_chest", "railways:sequenced_assembly/track_phantom", "scalinghealth:heart_crystal", "scalinghealth:heart_dust", "scalinghealth:power_crystal", "scalinghealth:bandages", "scalinghealth:medkit", "silentgear:bronze_ingot", "tfmg:filling/bottle_of_battery_acid", "tfmg:filling/bottle_of_concrete", "torchmaster:dreadlamp", "torchmaster:feral_flare_lantern", "torchmaster:frozen_pearl", "torchmaster:megatorch", "utilitix:ender_cart",];
     for (let ID of recipeID) {
@@ -7,8 +16,8 @@ ServerEvents.recipes(event => {
 
 // essentially i need to replace vanilla furnace recipes and replace them with the new nugget setup, then make mek recipes different to prevent those recipes from being changed https://github.com/mekanism/Mekanism/wiki/Recipe-Type-JSON-Syntax#smelting
 ServerEvents.recipes(event => {
-    let mekOreRecipe = ["iron", "copper", "gold", "zinc", "nickel", "lithium", "osmium", "tin", "lead", "uranium", "crimson_iron", "azure_silver", "cloggrum", "froststeel"]
-    for (let each of mekOreRecipe) {
+    let materials = ["iron", "copper", "gold", "zinc", "nickel", "lithium", "osmium", "tin", "lead", "uranium", "crimson_iron", "azure_silver", "cloggrum", "froststeel"]
+    for (let each of materials) {
         // Removal of every smelting and blasting recipe without harming other mods
         event.remove({ id: "minecraft:" + each + "_ingot_from_smelting_deepslate_" + each + "_ore" })
         event.remove({ id: "minecraft:" + each + "_ingot_from_blasting_deepslate_" + each + "_ore" })
@@ -22,6 +31,8 @@ ServerEvents.recipes(event => {
         event.remove({ id: "create:blasting/" + each + "_ingot_from_crushed" })
         event.remove({ id: "create:smelting/ingot_" + each + "_compat_mekanism" })
         event.remove({ id: "create:blasting/ingot_" + each + "_compat_mekanism" })
+        event.remove({ id: "create:splashing/crushed_raw_" + each })
+        event.remove({ id: "create:splashing/mekanism/crushed_raw_" + each })
         event.remove({ id: "mekanism:processing/" + each + "/ingot/from_ore_smelting" })
         event.remove({ id: "mekanism:processing/" + each + "/ingot/from_ore_blasting" })
         event.remove({ id: "mekanism:processing/" + each + "/ingot/from_raw_smelting" })
@@ -48,26 +59,45 @@ ServerEvents.recipes(event => {
         event.remove({ id: "jaopca:dusts.to_material_blasting." + each })
         event.remove({ id: "jaopca:create.crushed_to_material_smelting." + each })
         event.remove({ id: "jaopca:create.crushed_to_material_blasting." + each })
-        // Add new recipes in the altered output system
-        // - 1 Raw in Furnace = 2 Nuggets
-        // - 1 Raw Washed with Water = 2-4 Nuggets
-        // - 1 Raw in Create Grinding Wheels = 1-3 Crushed:
-        //   - 1 Crushed in Furnace = 3 Nuggets
-        //   - 1 Crushed Washed with Water = 3-4 Nuggets
-        //   - 1 Crushed in Create Millstone = 1-2 Dust:
-        //     - 1 Dust in Furnace = 4 Nuggets
-        //     - 1 Dust Washed with Water = 3-5 Nuggets
+        event.remove({ id: "jaopca:create.crushed_to_nugget." + each })
 
         // This call utilizes the secondary smelting list rather than the default one from base minecraft
         // event.recipes.mekanism.smelting(result, input)
         event.recipes.mekanism.smelting("#forge:ingots/" + each, "#forge:dusts/" + each)
-}})
+        console.log("LOADED MATERIAL: "+each)
+        // Add new recipes in the altered output system
+        // - 1 Raw in Furnace = 2 Nuggets
+        event.smelting("2x #forge:nuggets/" + each, "#forge:raw_materials/" + each)
+        event.blasting("2x #forge:nuggets/" + each, "#forge:raw_materials/" + each)
+
+        // - 1 Raw Washed with Water = 2-4 Nuggets
+
+        // - 1 Raw in Create Grinding Wheels = 1-3 Crushed:
+        event.recipes.create.milling("#create:crushed/" + each, "#forge:raw_materials/" + each)
+
+        // - 1 Crushed in Furnace = 3 Nuggets
+        event.smelting("3x #forge:nuggets/" + each, "#create:crushed/" + each)
+        event.blasting("3x #forge:nuggets/" + each, "#create:crushed/" + each)
+
+        // - 1 Crushed Washed with Water = 3-4 Nuggets
+
+        //  1 Crushed in Create Millstone = 1-2 Dust:
+        event.recipes.create.milling("#forge:dusts/" + each, "#create:crushed/" + each)
+
+        // - 1 Dust in Furnace = 4 Nuggets
+        event.smelting("4x #forge:nuggets/" + each, "#forge:dusts/" + each)
+        event.blasting("4x #forge:nuggets/" + each, "#forge:dusts/" + each)
+
+        // - 1 Dust Washed with Water = 3-5 Nuggets
+
+    }
+})
 
 ServerEvents.tags("item", event => {
     // Get all the raw_matertials as an array
     let oreList = event.get("forge:raw_materials").getObjectIds().map(id => id.toString());
 
-    // List the entries that we don't want
+    // List the entries that we dont want
     let exclusions = ["tfmg:raw_lead"];
     let newOreList = [];
     // Remove the exclusions from the list
